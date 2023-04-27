d6b281e653668fefb66f6192227a668d436f7db9
A <micoliariel@gmail.com>
2023-04-27 19:59:51 -0300

    Generacion componentes

---
+++ Frontend/ng_feria/src/app/components/shop/products-container/products-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-container',
+  templateUrl: './products-container.component.html',
+  styleUrls: ['./products-container.component.css']
+})
+export class ProductsContainerComponent {
+
+}
