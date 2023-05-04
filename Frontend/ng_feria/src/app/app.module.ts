d0de786f79854cae65b5fe4269bee8caecb8486e
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-05-03 23:32:53 -0300

    Update app.module.ts

--- Frontend/ng_feria/src/app/app.module.ts
+++ Frontend/ng_feria/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ProductsComponent } from './components/shop/products/products.component';
+import { ProductsContainerComponent } from './components/shop/products-container/products-container.component';
+import { CheckoutComponent } from './components/shop/checkout/checkout.component';
+import { CheckoutCardComponent } from './components/shop/checkout-card/checkout-card.component';
+import { DetailsComponent } from './components/shop/details/details.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //importe el módulo de formularios
   ],
   providers: [],
   bootstrap: [AppComponent]
