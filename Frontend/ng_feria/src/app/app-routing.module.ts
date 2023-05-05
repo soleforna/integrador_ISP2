86c2ccff337a9f4279ee432c3074768a606a4e4d
Marcos Barey <caposelmarco@gmail.com>
2023-05-04 22:04:30 -0300

    subiendo componente checkout

--- Frontend/ng_feria/src/app/app-routing.module.ts
+++ Frontend/ng_feria/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CheckoutComponent } from './components/shop/checkout/checkout.component';
+const routes: Routes = [
+   { path: 'checkout', component:CheckoutComponent }
+   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
