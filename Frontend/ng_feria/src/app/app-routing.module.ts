e299c6e7806f0034f3deb04f3f60d53b93a939ba
Vale L <v.lysikova@gmail.com>
2023-05-07 09:34:48 -0300

    Agregando modificaciones: nav, login, routing

--- Frontend/ng_feria/src/app/app-routing.module.ts
+++ Frontend/ng_feria/src/app/app-routing.module.ts
@@ -9,13 +9,16 @@ import { CheckoutComponent } from './components/shop/checkout/checkout.component
 import { ProductsComponent } from './components/shop/products/products.component';
 import { ProductsContainerComponent } from './components/shop/products-container/products-container.component';
 import { DetailsComponent } from './components/shop/details/details.component';
-//import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/auth/login/login.component';
 //import { RegisterComponent } from './components/register/register.component';
 //import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
-
+  {path:'', redirectTo:'/inicio', pathMatch:'full'},
+  {path:'inicio', component:LandingComponent},
+  {path:'iniciar-sesion',component:LoginComponent},
+  {path:'tienda',component:ShopComponent},
   { path: 'products', component: ProductsComponent},
   { path: 'Productos', component: ProductsContainerComponent},
   {path: 'checkout', component: CheckoutComponent},
