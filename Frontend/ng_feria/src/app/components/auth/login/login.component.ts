11fda0d081979f5b97d1c9026b1f23a22c1503ce
Vale L <v.lysikova@gmail.com>
2023-05-19 16:52:07 -0300

    Agregando json

--- Frontend/ng_feria/src/app/components/auth/login/login.component.ts
+++ Frontend/ng_feria/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm = this.formBuilder.group({
+    email:['usuario@gmail.com',[Validators.required, Validators.email]],
+    password:['',Validators.required]
+  })
+  constructor(private formBuilder:FormBuilder, private router:Router){}
+
+  ngOnInit(): void {
+
+  }
 
 }
