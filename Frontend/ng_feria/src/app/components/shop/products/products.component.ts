a7c36cdf06b24997d0d37b3a748a9dcaa87a89da
A <micoliariel@gmail.com>
2023-05-21 12:10:13 -0300

    hasta acá funciono consumo de api

--- Frontend/ng_feria/src/app/components/shop/products/products.component.ts
+++ Frontend/ng_feria/src/app/components/shop/products/products.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/Interfaces/product.interface';
+
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products: Product[] = []; 
 
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get<Product[]>('http://localhost:3000/products').subscribe(data => {
+      this.products = data;
+  })
 }
+
+  /*
+  hoy = new Date(); 
+  mostrarMovimientos: boolean=true; 
+  movimientos: any; 
+
+constructor(public loadProducts: ProductsService){
+  this.loadProducts.obtenerProductos().subscribe({
+    next: (productos) =>{
+      this.movimientos = productos 
+    }, 
+    error: (errorData) =>{
+      console.error(errorData); 
+    }
+  })
+}
+
+products: Product[] = db.products;
+*/
+}
+
+
---
+++ Frontend/ng_feria/src/app/services/products.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../Interfaces/product.interface';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  url:String = "http://localhost:3000/"
+  constructor(private http:HttpClient) { 
+    console.log("Servicio corriendo");
+    
+  }
+
+  obtenerProductos(): Observable <any>{
+    return this.http.get(this.url+"products")
+  }
+
+
+
+  
+}
