7d5d0e24649d3f762487a5eebeb3008fcdf16ef5
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-05-20 22:46:14 -0300

    Change language
    Change to Spanish
    time Zone UTC-3

--- Backend/drf_feriaonline/settings.py
+++ Backend/drf_feriaonline/settings.py
@@ -135,9 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
