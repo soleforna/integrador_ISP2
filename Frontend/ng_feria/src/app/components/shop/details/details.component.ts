0dec9d2a1e9724f4d4c7d8e9ada3f5c174f181a4
Joana <106889601+JoanaGM44@users.noreply.github.com>
2023-05-22 01:06:06 -0300

    se completa el componente ts de la vista detalles

--- Frontend/ng_feria/src/app/components/shop/details/details.component.ts
+++ Frontend/ng_feria/src/app/components/shop/details/details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../Interfaces/product.interface';
+import { ProductsService } from '../../../services/products.service';
+
+interface RouteParams {
+  category: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
+  categoryDetail!: string;
+  idDetail!: string;
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.categoryDetail = this.route.snapshot.paramMap.get('category')!;
+    this.idDetail = this.route.snapshot.paramMap.get('id')!;
+    this.productsService.obtenerProductos().subscribe((products: Product[]) => {
+      this.product = products.find(
+        (product: Product) =>
+          product.category === this.categoryDetail && 
+          product.id.toString() === this.idDetail
+      );
+    });
+  }
 
 }
