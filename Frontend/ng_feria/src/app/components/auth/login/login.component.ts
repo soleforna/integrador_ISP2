c81e4ef76f184bdd7eb7497806d4aee71d18cf44
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-05-28 01:55:28 -0300

    Update login.component.ts

--- Frontend/ng_feria/src/app/components/auth/login/login.component.ts
+++ Frontend/ng_feria/src/app/components/auth/login/login.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component } from "@angular/core";
+import { UsersService } from "src/app/services/users.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
- 
- 
-
-  constructor(private http: HttpClient) { }
+  email: string = "";
+  password: string = "";
 
 
-  loginWithGoogle(provider: string) {
-    window.location.href = `http://127.0.0.1:8000/accounts${provider}/login/`;
+  constructor(public userService: UsersService, public router:Router) {
+    // Verifica si el usuario está logueado.
+
+  }
+
+  login() {
+    const user = { email: this.email, password: this.password };
+    this.userService.login(user).subscribe((data: any) => {
+      localStorage.setItem("token", data.key);
+      this.userService.getUser(); // Obtiene el usuario y lo guarda en el localStorage.
+      //TODO: habria que poner un loader
+      this.router.navigate(["/producto"]); // Redirecciona a la página de productos.
+    }, (error: any) => { console.log(error) },
+
+    );
   }
 }
