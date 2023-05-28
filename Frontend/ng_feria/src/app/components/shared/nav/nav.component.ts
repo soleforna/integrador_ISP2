33b921e74a466206d9ab7dece59504013cae2ed8
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-05-28 01:56:35 -0300

    Update nav.component.ts
    Se crean metodos para obtener el nombre de usuario
    se oculta el boton de ingreso si hay un usuario logeado y se muestra el acceso al menu de usuarios

--- Frontend/ng_feria/src/app/components/shared/nav/nav.component.ts
+++ Frontend/ng_feria/src/app/components/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  user = JSON.parse(localStorage.getItem("user") || "{}");
+  name: string = this.getName(this.user) ;
+  isLoggedIn: boolean = false;
+
+  constructor(public userService: UsersService) {
+    // Verifica si el usuario está logueado.
+    this.isLoggedIn = localStorage.getItem('token') !== null;
+  }
+
+  // Obtiene el nombre del usuario.
+  getName(user: any) {
+    // Si el usuario no tiene nombre, entonces muestra su email.
+    if (user.first_name == '' || user.last_name == '' || user.first_name == null || user.last_name == null) {
+      return user.email;
+    }else{return user.first_name+' '+ user.last_name;}// Si el usuario tiene nombre, entonces muestra su nombre.
+  }
+
+  // Cierra la sesión del usuario.
+  logout() {
+    this.userService.logout();//llama al servicio para cerrar la sesion
+    window.location.reload();// Recarga la página.
+  }
+
+
 
 }
+
+
