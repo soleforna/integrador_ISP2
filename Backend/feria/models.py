82217f75109595861d906fc42663ca1433d2e462
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-05-29 01:47:32 -0300

    Update models.py

--- Backend/feria/models.py
+++ Backend/feria/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -23,16 +24,18 @@ class Article(models.Model):
         return self.name
 
 class Client(models.Model):
-    name = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def getEmail(self):
         return f"{self.user.email}"
-       
+    
+    def getFullName(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
     def __str__(self):
         return self.name
 
