bad8bbc50e96298530cc3e24a7355fa38197637f
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-05-28 21:13:04 -0300

    Update urls.py
    Se agrega el patron de url para ver los archivos estaticos

--- Backend/drf_feriaonline/urls.py
+++ Backend/drf_feriaonline/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.registration.views import RegisterView
@@ -32,3 +34,5 @@ urlpatterns = [
     path('api/auth/google/', GoogleLogin.as_view(), name="google_login")
     #path('accounts', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
