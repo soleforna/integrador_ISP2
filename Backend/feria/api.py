41aa29c26339061bcace4ffbd71a0eed7b16ef48
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-05-29 01:47:24 -0300

    Update api.py
    Se crea un metodo update para apuntar al verbo PUT de Client

--- Backend/feria/api.py
+++ Backend/feria/api.py
@@ -1,11 +1,15 @@
 from .models import *
-from rest_framework import viewsets, permissions
 from .serializers import *
+from django.contrib.auth.hashers import check_password
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.filter(stock__gt=0) #de esta manera solo se muestran los articulos que tienen stock
     permission_classes = [permissions.AllowAny]
-    serializer_class = ArticleSerializer 
+    serializer_class = ArticleSerializer
+    lookup_field = 'pk' #para que se pueda buscar por id en la url
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -31,3 +35,45 @@ class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ClientSerializer
+    lookup_field = 'pk' #para que se pueda buscar por id en la url
+    
+    def update(self, request, pk=None):
+        client = self.get_object()
+        user = client.user
+
+        user_fields = ['email', 'username', 'first_name', 'last_name', 'password']
+        for field in user_fields:
+            if field in request.data and request.data[field] != 'null':
+                if field == 'password':
+                    if check_password(request.data[field], user.password):
+                        raise ValidationError("La contraseña es igual a la actual.")
+                    else:
+                        user.set_password(request.data[field])
+                else:
+                    setattr(user, field, request.data[field])
+
+        # Actualizar los campos del modelo Client solo si se envían en la solicitud
+        client_fields = ['phone', 'address', 'avatar']
+        for field in client_fields:
+            if field in request.data and request.data[field] != 'null':
+                setattr(client, field, request.data[field])
+
+        # Guardar los cambios en los modelos User y Client
+        user.save()
+        client.save()
+
+        serializer = ClientSerializer(client, data=request.data, partial=True) #partial=True para que no sea obligatorio enviar todos los campos
+        user_serializer = UserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid() and user_serializer.is_valid(): #si los serializers son validos
+            serializer.save() #se guardan los datos
+            user_serializer.save()
+            response = { #se crea un diccionario con los datos actualizados
+                'user': user_serializer.data,
+                'client': serializer.data
+            }
+            return Response(response, status=status.HTTP_200_OK) #se devuelve el el diccionario con un status 200
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
