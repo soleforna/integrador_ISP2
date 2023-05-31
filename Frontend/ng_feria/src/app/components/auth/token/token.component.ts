b3c2080f63fabfb5e1e9a8f9839bb9c4f038b9e1
Lautaro palacio <pala797@gmail.com>
2023-05-30 22:42:08 -0300

    agrege componentes para hacer el token

---
+++ Frontend/ng_feria/src/app/components/auth/token/token.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-token',
+  templateUrl: './token.component.html',
+  styleUrls: ['./token.component.css']
+})
+
+
+export class TokenComponent{
+
+}
