b31eeb67bfd8d996ffc93c27b493858f05e24aa8
Marcos Barey <106411609+Marquitos280419@users.noreply.github.com>
2023-06-04 18:16:47 -0300

    subiendo mostrar reseñas y pequeños cambios

--- Frontend/ng_feria/src/app/components/landing/landing.component.ts
+++ Frontend/ng_feria/src/app/components/landing/landing.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReseñaService } from "src/app/services/reseñas.service";
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +8,35 @@ import { Component} from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  constructor(private ReseñaService: ReseñaService,){ }
+  resenas: any[] = [];
+  imagenPredefinida = '../../../../assets/img/user.png';
+
+  ngOnInit(): void {
+    this.obtenerReseñas();
+  }
+
+  obtenerReseñas(): void {
+    this.ReseñaService.getReseñas().subscribe(
+      (resenas) => {
+        this.resenas = resenas;
+      },
+      (error) => {
+        // Manejo de errores
+      }
+    );
+  }
+
+  getEstrellas(clasificacion: number): number[] {
+    return Array(clasificacion).fill(0);
+  }
+  getAvatarImage(client_Avatar: string): string { //paso como argumento el client_avatar
+    return client_Avatar ? client_Avatar : '../../../../assets/img/user.png'; //retorno la imagen que trae y si no le paso una predefinida
+  }
+  getNombre(client_name: string): string {
+    return client_name ? client_name : 'Anónimo';
+  }
+
 
   isScrolled = false;
 
@@ -13,5 +44,5 @@ export class LandingComponent {
   scroll(el:HTMLElement){
     el.scrollIntoView()
   }
-  
+
 }
