5d7908620a88ab935ca95668ebde95abea11186b
Marcos Barey <106411609+Marquitos280419@users.noreply.github.com>
2023-06-07 21:36:02 -0300

    validaciones en el nav

--- Frontend/ng_feria/src/app/components/shared/nav/nav.component.ts
+++ Frontend/ng_feria/src/app/components/shared/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 import { UsersService } from "src/app/services/users.service";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav',
@@ -11,9 +13,9 @@ export class NavComponent {
   user = JSON.parse(localStorage.getItem("user") || "{}");
   name: string = this.getName(this.user) ;
   isLoggedIn: boolean = false;
-  avatar = '../../../../assets/img/user.png';
+  avatar1 = '../../../../assets/img/profile.png';
 
-  constructor(public userService: UsersService) {
+  constructor(public userService: UsersService,private router: Router) {
     // Verifica si el usuario está logueado.
     this.isLoggedIn = localStorage.getItem('token') !== null;
   }
@@ -25,7 +27,25 @@ export class NavComponent {
       return user.email;
     }else{return user.first_name +' '+ user.last_name;} // Si el usuario tiene nombre, entonces muestra su nombre.
   }
- 
+
+  getAvatar(user: any) {
+    if (!user.client_avatar || user.client_avatar === '') {
+      return this.avatar1; // Si el usuario no tiene avatar, entonces muestra un avatar predefinido.
+    } else {
+      return user.client_avatar; // Si el usuario tiene avatar, entonces muestra su avatar.
+    }
+  }
+  isProductsPage(): boolean {
+    const url = this.router.url;
+    return url.includes('producto')  || url.includes('productos')  || url.includes('tienda')  || url.includes('checkout')
+      || url.includes('detalle')  || url.includes('checkout-card'); //llamo a los componentes
+       // si esta en estos componentes muestra los enlases de la tienda
+  }
+
+  isLandingPage(): boolean {
+    return this.router.url.includes('inicio');// si esta en inicio muestra los enlsases del landig
+  }
+
 
 
   // Cierra la sesión del usuario.
