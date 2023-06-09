e05706bb4fbc856c45bf5a8bef341cc48771af01
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-06-09 00:31:52 -0300

    Details
    Se agregan las reseñas al detalle articulo

--- Frontend/ng_feria/src/app/services/products.service.ts
+++ Frontend/ng_feria/src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../Interfaces/product.interface';
 
@@ -18,8 +18,18 @@ export class ProductsService {
     return this.http.get(this.url+"articles")
   }
 
+  agregarReview(id: number, data: any): Observable<any> {
+    const tk = localStorage.getItem("token");
+    let headers = new HttpHeaders();
+    if (tk) {
+      headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + tk,
+        'Content-type': 'application/json'
+      });}
 
+      return this.http.post(this.url + "articles/" + id + "/add_review/", data, { headers });
 
+  }
 
 }
 
