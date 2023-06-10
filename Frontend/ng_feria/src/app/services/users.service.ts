91a00c18963b14c5de0b1fec7a9cd94a4315677f
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-06-09 21:38:37 -0300

    Update users.service.ts
    actualizarUsuario

--- Frontend/ng_feria/src/app/services/users.service.ts
+++ Frontend/ng_feria/src/app/services/users.service.ts
@@ -1,29 +1,34 @@
-
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
-
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class UsersService {
   constructor(private http: HttpClient) {}
 
-
   login(user: any) {
-    return this.http.post("http://localhost:8000/api/auth/login/", {
+    return this.http.post('http://localhost:8000/api/auth/login/', {
       email: user.email,
-      password: user.password
+      password: user.password,
     });
   }
 
- // register(formData: any) {
-    //return this.http.post("http://localhost:8000/api/auth/register/", {
+  // register(formData: any) {
+  //return this.http.post("http://localhost:8000/api/auth/register/", {
 
-    //});
+  //});
   //}
-  register(first_name: string, last_name: string, username: string, email: string, password: string, phone: string, address: string): Observable<any> {
+  register(
+    first_name: string,
+    last_name: string,
+    username: string,
+    email: string,
+    password: string,
+    phone: string,
+    address: string
+  ): Observable<any> {
     const formData = new FormData();
     formData.append('first_name', first_name);
     formData.append('last_name', last_name);
@@ -37,37 +42,52 @@ export class UsersService {
   }
 
   logout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    return this.http.post("http://localhost:8000/api/auth/logout/", {'Authorization: Token ': localStorage.getItem("token")});
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return this.http.post('http://localhost:8000/api/auth/logout/', {
+      'Authorization: Token ': localStorage.getItem('token'),
+    });
   }
 
   // Obtiene el usuario y lo guarda en el localStorage.
   getUser() {
-    const tk = localStorage.getItem("token");
+    const tk = localStorage.getItem('token');
 
-    if (tk) { // Si existe un token en el localStorage, entonces lo envía en el header de la petición.
+    if (tk) {
+      // Si existe un token en el localStorage, entonces lo envía en el header de la petición.
       const headers = new HttpHeaders({
-        'Authorization': 'Token ' + tk,
-        'Content-type': 'application/json'
+        Authorization: 'Token ' + tk,
+        'Content-type': 'application/json',
       });
 
-      this.http.get("http://localhost:8000/api/auth/user/", { headers }).subscribe(
-        (res: any) => {
-          localStorage.setItem("user", JSON.stringify(res)); // Guarda el usuario en el localStorage.
-        }, (err: any) => {
-          return err;
-        });
+      this.http
+        .get('http://localhost:8000/api/auth/user/', { headers })
+        .subscribe(
+          (res: any) => {
+            localStorage.setItem('user', JSON.stringify(res)); // Guarda el usuario en el localStorage.
+          },
+          (err: any) => {
+            return err;
+          }
+        );
       return true;
-
-    }else{
+    } else {
       // El token no está disponible en el localStorage. Realiza alguna acción adicional o muestra un mensaje de error.
       return false;
     }
   }
 
+  /* Actualizo los datos del usuario */
+  actualizarUsuario(datos: any): Observable<any> {
+
+    const url = `http://localhost:8000/api/auth/user/${datos.pk}`;
+    const body = JSON.stringify(datos);
+
+    return this.http.patch(url, body).pipe(
+      catchError((error) => {
+        // Manejo de errores
+        throw error;
+      })
+    );
+  }
 }
-
-
-
-
