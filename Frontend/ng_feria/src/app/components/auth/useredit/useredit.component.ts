75b3b051cffdac6e73de77aac4187b99ec88542e
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-06-11 18:58:23 -0300

    Componente editar usuario
    Se agregan los campos provincia y municipio en el select consumiendo datos del servicio

--- Frontend/ng_feria/src/app/components/auth/useredit/useredit.component.ts
+++ Frontend/ng_feria/src/app/components/auth/useredit/useredit.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
-
+import { DatosgeograficosService } from 'src/app/services/datosgeograficos.service';
 
 @Component({
   selector: 'app-useredit',
   templateUrl: './useredit.component.html',
-  styleUrls: ['./useredit.component.css']
+  styleUrls: ['./useredit.component.css'],
 })
 export class UsereditComponent implements OnInit {
   formData: any;
   calle: string | undefined;
   ciudad: string | undefined;
-  provincia: string | undefined;
+  provinciaActual: string | undefined;
+  provincias: string[] = [];
+  municipios: any;
 
-
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private dg: DatosgeograficosService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit() {
     const storedData = localStorage.getItem('user');
@@ -24,11 +28,66 @@ export class UsereditComponent implements OnInit {
       const resultado = this.dividirDireccion(direccion);
       this.calle = resultado.calle;
       this.ciudad = resultado.ciudad;
-      this.provincia = resultado.provincia;
+      this.provinciaActual = resultado.provincia;
+      console.log(this.ciudad)
     }
+    
+
+    this.dg.obtenerDatosProvincias().subscribe(
+      (response) => {
+        /* Guardo las provincias en el local storage */
+        const provinciasString = JSON.stringify(response);
+        localStorage.setItem('provincias', provinciasString);
+        
+
+        if (provinciasString) {
+          const provinciasObj = JSON.parse(provinciasString);
+
+          const nombresProvincias = provinciasObj.provincias.map(
+            (provincia: any) => provincia.nombre
+          );
+          this.provincias = nombresProvincias;
+
+          if (this.provinciaActual) {
+            
+            this.dg.obtenerDatosMunicipio(this.provinciaActual).subscribe(
+              (response) => {
+                
+                /* Guardo los municipios en el local storage */
+                const municipiosString = JSON.stringify(response);
+                localStorage.setItem('municipios', municipiosString);
+
+                if (municipiosString){
+                  const municipiosObj = JSON.parse(municipiosString);
+                  const nombresMunicipios = municipiosObj.municipios.map(
+                    (municipios:any) => municipios.nombre
+                  );
+                  this.municipios = nombresMunicipios;
+                  console.log(this.municipios) 
+                }
+                
+                 
+
+                console.log(response);
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
+          }
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  dividirDireccion(address: string): { calle: string; ciudad: string; provincia: string } {
+  dividirDireccion(address: string): {
+    calle: string;
+    ciudad: string;
+    provincia: string;
+  } {
     const partes: string[] = address.split(',');
     const calle: string = partes[0].trim();
     const ciudad: string = partes[1].trim();
@@ -37,15 +96,12 @@ export class UsereditComponent implements OnInit {
     return {
       calle: calle,
       ciudad: ciudad,
-      provincia: provincia
+      provincia: provincia,
     };
   }
 
-
-
   submitForm() {
-    console.log("Estoy en submit");
-
+    console.log('Estoy en submit');
 
     const domicilio =
       (document.getElementById('inputAddress') as HTMLInputElement)?.value +
@@ -53,26 +109,29 @@ export class UsereditComponent implements OnInit {
       (document.getElementById('inputCity') as HTMLSelectElement)?.value +
       ', ' +
       (document.getElementById('inputState') as HTMLSelectElement)?.value;
-      console.log(domicilio);
+    console.log(domicilio);
 
     const datos = {
       id: localStorage.getItem('user.pk'),
-      first_name: (document.getElementById('firstName') as HTMLInputElement)?.value,
-      last_name: (document.getElementById('lastName') as HTMLInputElement)?.value,
+      first_name: (document.getElementById('firstName') as HTMLInputElement)
+        ?.value,
+      last_name: (document.getElementById('lastName') as HTMLInputElement)
+        ?.value,
       email: (document.getElementById('Email') as HTMLInputElement)?.value,
-      password: (document.getElementById('inputPassword') as HTMLInputElement)?.value,
+      password: (document.getElementById('inputPassword') as HTMLInputElement)
+        ?.value,
       domicilio: domicilio,
-      telefono: (document.getElementById('phone') as HTMLInputElement)?.value
+      telefono: (document.getElementById('phone') as HTMLInputElement)?.value,
     };
     console.log(datos);
     this.usersService.actualizarUsuario(datos).subscribe(
       (response) => {
         console.log(response);
-        localStorage.setItem("user", JSON.stringify(response));
+        localStorage.setItem('user', JSON.stringify(response));
       },
       (error) => {
         // Manejo de errores
-        console.log(error)
+        console.log(error);
       }
     );
   }
