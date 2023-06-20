e40be13cef9f76a48e3bed5eb76b73160eeaad7f
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-06-19 21:15:50 -0300

    Update products.component.ts

--- Frontend/ng_feria/src/app/components/shop/products/products.component.ts
+++ Frontend/ng_feria/src/app/components/shop/products/products.component.ts
@@ -25,10 +25,12 @@ export class ProductsComponent implements OnInit {
     );
   }
 
-  //agregar al carrito
-  addCart(): void {
-    console.log('Agregando al carrito con el siguiente ID de articulo: ' +this.idProduct);
-    this.cartService.addArticleToCart(parseInt(this.idProduct), //le paso el id del producto
-    )
+    //metodo para agregar al carrito
+  addCart(productId: string): void {
+    console.log('Agregando al carrito con el siguiente ID de articulo: ' + productId);
+    this.cartService.addArticleToCart( //llamo al servicio para agregar al carrito
+      parseInt(productId) //le paso el id del producto
+    );
   }
+
 }
