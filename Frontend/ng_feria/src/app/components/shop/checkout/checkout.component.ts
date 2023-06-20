bd5b8b20674a0097a7ad9b60de3d34ad55bc6fd3
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-06-20 20:22:07 -0300

    Agregando funcionalidad Paypal
    Se agrega funcionalidad paypal

--- Frontend/ng_feria/src/app/components/shop/checkout/checkout.component.ts
+++ Frontend/ng_feria/src/app/components/shop/checkout/checkout.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  IPayPalConfig,
+  ICreateOrderRequest
+} from 'ngx-paypal';
 
 
 
@@ -10,9 +17,80 @@ import { CommonModule } from '@angular/common';
 
 }
 )
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
      iva=21;
      descuento=200;
+
+     public payPalConfig ? : IPayPalConfig;
+
+     ngOnInit(): void {
+         this.initConfig();
+     }
+
+     private initConfig(): void {
+      this.payPalConfig = {
+          currency: 'EUR',
+          clientId: 'AZUX7uAdkRgGvnX67Uqou1oZW1FhEnCsFtzJKBIrywdP5DZDB0VHaRDpoEpghSFTAFsiNPoLGEBuOE1W',
+          createOrderOnClient: (data) => < ICreateOrderRequest > {
+              intent: 'CAPTURE',
+              purchase_units: [{
+                  amount: {
+                      currency_code: 'EUR',
+                      value: '9.99',
+                      breakdown: {
+                          item_total: {
+                              currency_code: 'EUR',
+                              value: '9.99'
+                          }
+                      }
+                  },
+                  items: [{
+                      name: 'Enterprise Subscription',
+                      quantity: '1',
+                      category: 'DIGITAL_GOODS',
+                      unit_amount: {
+                          currency_code: 'EUR',
+                          value: '9.99',
+                      },
+                  }]
+              }]
+          },
+          advanced: {
+              commit: 'true'
+          },
+          style: {
+              label: 'paypal',
+              layout: 'vertical'
+          },
+          onApprove: (data: any, actions: any) => {
+            console.log('onApprove - transaction was approved, but not authorized', data, actions);
+            actions.order.get().then((details: any) => {
+              console.log('onApprove - you can get full order details inside onApprove: ', details);
+            });
+          },
+
+
+      
+          onClientAuthorization: (data) => {
+              console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+
+          },
+          onCancel: (data, actions) => {
+              console.log('OnCancel', data, actions);
+
+
+          },
+          onError: err => {
+              console.log('OnError', err);
+
+          },
+          onClick: (data, actions) => {
+              console.log('onClick', data, actions);
+
+          }
+      };
+  }
+
 
      products = [
     {
