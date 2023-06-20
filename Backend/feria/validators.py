f6353aa616260e98397fb0b51f6dd3464ebf94e3
Marcos Barey <106411609+Marquitos280419@users.noreply.github.com>
2023-06-20 10:57:19 -0300

    Update validators.py
    se agrego webp al regex y también caracteres

--- Backend/feria/validators.py
+++ Backend/feria/validators.py
@@ -12,7 +12,7 @@ def phone_valid(phone): #validar el telefono
     validate_field(str(phone), regex, error_message) #validar el campo
 
 def name_valid(first_name): #validar el nombre
-        regex = r'^[a-zA-Z\s]{3,}$' #expresion regular para validar el nombre
+        regex = r'^[a-zA-ZñÑ\s]{3,}$' #expresion regular para validar el nombre
         error_msg = 'El nombre debe tener al menos 3 caracteres y solo puede contener letras y espacios.' #mensaje de error
         validate_field(first_name, regex, error_msg) #validar el campo
         
@@ -22,6 +22,6 @@ def address_valid(address): #validar la direccion
         validate_field(address, regex, error_msg) #validar el campo
 
 def validate_image(image):
-    regex = r'^.*\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$'
+    regex = r'^.*\.(jpg|jpeg|png|webp|gif|JPG|JPEG|PNG|WEBP|GIF)$'
     error_msg = 'El archivo no es una imagen válida.'
     validate_field(image.name, regex, error_msg)
