140654dde3d24a3de9ecfb83d5a357d266c80964
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-06-19 21:00:44 -0300

    Update validators.py
    Se agregan validaciones de caracteres

--- Backend/feria/validators.py
+++ Backend/feria/validators.py
@@ -7,17 +7,17 @@ def validate_field(value, regex, error_message): #recibe el valor, la expresion 
         raise ValidationError(error_message) #se devuelve el mensaje de error
 
 def phone_valid(phone): #validar el telefono
-    regex = r'^[0-9]{8,12}$' #expresion regular para validar el telefono (8 a 12 digitos)
+    regex = r'^[+0-9]{8,12}$' #expresion regular para validar el telefono (8 a 12 digitos)
     error_message = 'El teléfono debe contener solo dígitos y tener entre 8 y 12 caracteres.' #mensaje de error
     validate_field(str(phone), regex, error_message) #validar el campo
 
 def name_valid(first_name): #validar el nombre
-        regex = r'^[a-zA-Z\s]{3,}$' #expresion regular para validar el nombre
+        regex = r'^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,}$' #expresion regular para validar el nombre
         error_msg = 'El nombre debe tener al menos 3 caracteres y solo puede contener letras y espacios.' #mensaje de error
         validate_field(first_name, regex, error_msg) #validar el campo
         
 def address_valid(address): #validar la direccion
-        regex = r'^[a-zA-Z0-9\s\.,#-]{5,}$' #expresion regular para validar la direccion (5 o mas caracteres)
+        regex = r'^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9\s\.,#-]{5,}$' #expresion regular para validar la direccion (5 o mas caracteres)
         error_msg = 'La dirección debe tener al menos 5 caracteres y solo puede contener letras, números, espacios y los caracteres .,#-' #mensaje de error
         validate_field(address, regex, error_msg) #validar el campo
 
