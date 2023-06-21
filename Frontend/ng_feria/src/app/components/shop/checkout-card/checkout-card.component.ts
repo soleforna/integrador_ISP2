badb92e251a41063f98c3ad9300e02a556a493fe
Maria Soledad Fornasier <informatica.ijo@gmail.com>
2023-06-21 17:20:14 -0300

    Confirmación de transacción
    Se agrega modal de compra realizada

--- Frontend/ng_feria/src/app/components/shop/checkout-card/checkout-card.component.ts
+++ Frontend/ng_feria/src/app/components/shop/checkout-card/checkout-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-checkout-card',
   templateUrl:'./checkout-card.component.html',
   styleUrls: ['./checkout-card.component.css']
 })
-export class CheckoutCardComponent {
+export class CheckoutCardComponent implements OnInit{
+  @Input() price: any;
+  @Input() items: any;
+
+  constructor(public activeModal: NgbActiveModal){}
+
+  ngOnInit(): void {
+
+    console.log('price is ', this.price);
+    console.log(this.items)
+
+  }
+
 
 }
