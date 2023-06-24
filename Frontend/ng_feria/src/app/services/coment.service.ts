e334f7e7963925c01c003c4db2af3a17adc6acb3
Marcos Barey <106411609+Marquitos280419@users.noreply.github.com>
2023-06-24 20:45:59 -0300

    Update landingComponent
    subiendo el agregar comentarios

--- Frontend/ng_feria/src/app/services/coment.service.ts
+++ Frontend/ng_feria/src/app/services/coment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,5 +12,26 @@ export class ComentService {
 
   getComent(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl); // hago un get a la api de comentarios
+  }
+  sendComents(description: string, classification: number): Observable<any> {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    const body = {
+      description: description,
+      classification: classification,
+      client_id: user.id
+    };
+
+    const tk = localStorage.getItem('token');
+
+    let headers = new HttpHeaders();
+    if (tk) {
+      headers = new HttpHeaders({
+        'Authorization': 'Bearer ' + tk,
+        'Content-Type': 'application/json' // Establece el tipo de contenido como JSON
+      });
+    }
+
+    return this.http.post(this.apiUrl, body, { headers });
+
   }
 }
